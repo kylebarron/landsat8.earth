d9bf299c1c47b4e93250a201f2e81f0bf31e04a9
Kyle Barron <barronk@mit.edu>
2020-04-25 21:03:51 -0600

    Add map of NAIP mosaic urls

---
+++ src/components/constants.js
@@ -0,0 +1,10 @@
+export const NAIP_MOSAICS = {
+  '2011-2013':
+    'dynamodb://us-west-2/74f48044f38db32666078e75f3439d8e62cf9e25820afc79ea6ce19f',
+  '2014-2015':
+    'dynamodb://us-west-2/5395d9e7bba4eeaa6af4842e1a7b9d3ea9dfc2a74373ae24698809e9',
+  '2015-2017':
+    'dynamodb://us-west-2/7610d6d77fca346802fb21b89668cb12ef3162a31eb71734a8aaf5de',
+  '2016-2018':
+    'dynamodb://us-west-2/94c61bd217e1211db47cf7f8b95bbc8e5e7d68a26cd9099319cf15f9',
+};
