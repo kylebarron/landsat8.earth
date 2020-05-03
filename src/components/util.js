81910815ef9724837a37ba1365ba7d7e5a9541a7
Kyle Barron <kylebarron2@gmail.com>
2020-05-02 20:58:41 -0600

    Parse view state from hash Ref #6

--- src/components/util.js
+++ src/components/util.js
@@ -96,3 +96,43 @@ export function getModisUrls(options) {
     'https://gibs-c.earthdata.nasa.gov/' + tilePath,
   ];
 }
+
+/**
+ * Get ViewState from page URL hash
+ * Note: does not necessarily return all viewState fields
+ * @param {string} hash Page URL hash
+ */
+export function getViewStateFromHash(hash) {
+  if (!hash || hash.charAt(0) !== '#') {
+    return;
+  }
+
+  // Split the hash into an array of numbers
+  let hashArray = hash
+    // Remove # symbol
+    .substring(1)
+    .split('/')
+    .map(Number);
+
+  // Remove non-numeric values
+  hashArray = hashArray.map(val => (Number.isFinite(val) && val) || null);
+
+  // Order of arguments:
+  // https://docs.mapbox.com/mapbox-gl-js/api/
+  const [zoom, latitude, longitude, bearing, pitch] = hashArray;
+  const viewState = {
+    bearing,
+    latitude,
+    longitude,
+    pitch,
+    zoom,
+  };
+
+  // Delete null keys
+  // https://stackoverflow.com/a/38340730
+  Object.keys(viewState).forEach(
+    key => viewState[key] == null && delete viewState[key]
+  );
+
+  return viewState;
+}
