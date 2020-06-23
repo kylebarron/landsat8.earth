7849b39a1e978e4df02c44abf6c09f9125f4411e
Kyle Barron <kylebarron2@gmail.com>
2020-06-22 22:07:07 -0600

    Update LandsatTerrainTile to use deck.gl-raster@0.2.0

--- src/components/deck-layers/landsat-terrain-tile.js
+++ src/components/deck-layers/landsat-terrain-tile.js
@@ -9,7 +9,11 @@ import {
   getMercatorModelMatrix,
   loadTerrain,
 } from './util';
-import { BandsSimpleMeshLayer } from '@kylebarron/deck.gl-extended-layers';
+import {
+  RasterMeshLayer,
+  combineBands,
+  pansharpenBrovey,
+} from '@kylebarron/deck.gl-raster';
 import { getLandsatUrl } from '../util';
 import { imageUrlsToTextures } from '../util/webgl';
 
@@ -84,13 +88,11 @@ async function getTileData(options) {
 
   // Load landsat urls
   const urls = [
+    pan ? getLandsatUrl({ x, y, z, bands: 8, mosaicUrl, color_ops }) : null,
     getLandsatUrl({ x, y, z, bands: rgbBands[0], mosaicUrl, color_ops }),
     getLandsatUrl({ x, y, z, bands: rgbBands[1], mosaicUrl, color_ops }),
     getLandsatUrl({ x, y, z, bands: rgbBands[2], mosaicUrl, color_ops }),
   ];
-  if (pan) {
-    urls.push(getLandsatUrl({ x, y, z, bands: 8, mosaicUrl, color_ops }));
-  }
 
   // const { textures, assets } = await imageUrlsToTextures(gl, urls);
   // const [awaited_textures, awaited_terrain] = Promise.all([
@@ -99,8 +101,8 @@ async function getTileData(options) {
   // ]);
   // return { assets, textures: awaited_textures, terrain: awaited_terrain };
 
-  const textures = imageUrlsToTextures(gl, urls);
-  return Promise.all([textures, terrain]);
+  const [imagePan, ...imageBands] = await imageUrlsToTextures(gl, urls);
+  return Promise.all([{ imageBands, imagePan }, terrain]);
 }
 
 function renderSubLayers(props) {
@@ -114,51 +116,31 @@ function renderSubLayers(props) {
 
   // Resolve promise if needed
   // Apparently when overzooming, data is provided not as a promise
-  let image_r;
-  let image_g;
-  let image_b;
-  let image_pan;
-  let mesh;
   let textures;
+  let mesh;
   if (Array.isArray(data)) {
     textures = data[0];
     mesh = data[1];
-
-    image_r = textures[0];
-    image_g = textures[1];
-    image_b = textures[2];
-    if (pan && textures.length === 4) {
-      image_pan = textures[3];
-    }
   } else if (data) {
     textures = data.then(result => result && result[0]);
     mesh = data.then(result => result && result[1]);
-
-    image_r = textures.then(result => result && result[0]);
-    image_g = textures.then(result => result && result[1]);
-    image_b = textures.then(result => result && result[2]);
-    if (pan) {
-      image_pan = textures.then(result => result && result[3]);
-    }
   }
 
-  return [
-    new BandsSimpleMeshLayer(props, {
-      // NOTE: currently you need to set each sublayer id so they don't conflict
-      // NOTE: is there an id on props?
-      id: `${props.id}-${tile.x}-${tile.y}-${tile.z}`,
-      data: DUMMY_DATA,
-      mesh,
-      image_r,
-      image_g,
-      image_b,
-      image_pan,
-      getPolygonOffset: null,
-      coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
-      modelMatrix: getMercatorModelMatrix(tile),
-      getPosition: d => [0, 0, 0],
-      // Color to use if surfaceImage is unavailable
-      getColor: [255, 255, 255],
-    }),
-  ];
+  const modules = [combineBands];
+  if (pan) {
+    modules.push(pansharpenBrovey);
+  }
+
+  return new RasterMeshLayer(props, {
+    data: DUMMY_DATA,
+    mesh,
+    modules,
+    asyncModuleProps: textures,
+    getPolygonOffset: null,
+    coordinateSystem: COORDINATE_SYSTEM.CARTESIAN,
+    modelMatrix: getMercatorModelMatrix(tile),
+    getPosition: d => [0, 0, 0],
+    // Color to use if surfaceImage is unavailable
+    getColor: [255, 255, 255],
+  });
 }
