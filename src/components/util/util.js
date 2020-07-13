4ea44c23b6850764c64aadde2a08dab2463f6996
Kyle Barron <kylebarron2@gmail.com>
2020-07-13 00:11:24 -0600

    Restore colormap previews

--- src/components/util/util.js
+++ src/components/util/util.js
@@ -3,5 +3,10 @@
 // Keep only the `key`, `text`, and `value` keys of an array of objects
 export function arrayToProps(array) {
   // https://stackoverflow.com/a/39333479
-  return array.map(({ key, text, value }) => ({ key, text, value }));
+  return array.map(({ key, text, value, image }) => ({
+    key,
+    text,
+    value,
+    image,
+  }));
 }
