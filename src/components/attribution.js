1346ccf90e0bbd03d41923fb3338d8d58d5e995b
Kyle Barron <kylebarron2@gmail.com>
2020-09-07 20:13:46 -0600

    Add rel noreferrer

--- src/components/attribution.js
+++ src/components/attribution.js
@@ -11,15 +11,14 @@ export default function AttributionDiv(props) {
         right: 0,
         bottom: 0,
         zIndex: 1,
-        font: '12px/20px Helvetica Neue,Arial,Helvetica,sans-serif;',
-        fontSize: '12px',
-        fontWeight: 300,
+        font: '12px/20px Helvetica Neue,Arial,Helvetica,sans-serif',
       }}
     >
       {showNaip && (
         <a
           href="https://www.fsa.usda.gov/programs-and-services/aerial-photography/imagery-programs/naip-imagery/"
           target="_blank"
+          rel="noreferrer"
         >
           © USDA{' '}
         </a>
@@ -28,6 +27,7 @@ export default function AttributionDiv(props) {
         <a
           href="https://www.usgs.gov/land-resources/nli/landsat"
           target="_blank"
+          rel="noreferrer"
         >
           © USGS/NASA Landsat{' '}
         </a>
@@ -36,14 +36,19 @@ export default function AttributionDiv(props) {
         <a
           href="https://lpdaac.usgs.gov/data/get-started-data/collection-overview/missions/modis-overview/"
           target="_blank"
+          rel="noreferrer"
         >
           © USGS/NASA MODIS{' '}
         </a>
       )}
-      <a href="http://www.openmaptiles.org/" target="_blank">
+      <a href="http://www.openmaptiles.org/" target="_blank" rel="noreferrer">
         © OpenMapTiles
       </a>{' '}
-      <a href="http://www.openstreetmap.org/about/" target="_blank">
+      <a
+        href="http://www.openstreetmap.org/about/"
+        target="_blank"
+        rel="noreferrer"
+      >
         © OpenStreetMap contributors
       </a>
     </div>
