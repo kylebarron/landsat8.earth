ef387954346839400957bcb42dc4920f55aafbb3
Kyle Barron <kylebarron2@gmail.com>
2020-09-07 21:39:47 -0600

    Update page title

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,7 +37,7 @@ function SEO({description, lang, meta, title}) {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      titleTemplate={`%s | ${metaDescription}`}
       meta={[
         {
           name: `description`,
