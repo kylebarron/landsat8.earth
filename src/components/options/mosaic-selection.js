800b0acb597b6049bf5c0261c28a829f35ce6ac9
Kyle Barron <kylebarron2@gmail.com>
2020-09-08 00:48:16 -0600

    Add mosaic selection description

--- src/components/options/mosaic-selection.js
+++ src/components/options/mosaic-selection.js
@@ -17,6 +17,17 @@ export default function MosaicSelection(props) {
         options={Object.values(LANDSAT_MOSAICS)}
         onChange={(event, object) => onChange({landsatMosaicId: object.value})}
       />
+      <br/>
+      <br/>
+      <p>
+        The <i>mosaic selection</i> defines how the server should join multiple Landsat images into seamless imagery for display.
+      </p>
+      <p>
+        This project focuses on prebuilt, low-cloud mosaics. Each seasonal mosaic is created by first selecting all images in the given region in the season, then using those with the least cloud percentage.
+      </p>
+      <p>
+        The "Latest Available" mosaic uses the most recent images available, regardless of cloud cover, while its cloudless counterpart uses the most recent images with less than 5% cloud cover.
+      </p>
 
       {/* Todo: don't show unless also at a zoom where NAIP is visible */}
       {useNaip && (
